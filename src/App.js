d2edb2b15dfa76707e6f43c010044ce2e129ac3f
mahamadi congo <mahamadicongo@hotmail.com>
2020-01-17 21:55:37 +0100

    Contextualize the content of the components

--- src/App.js
+++ src/App.js
@@ -3,18 +3,24 @@ import './App.css'
 import GuessCount from './GuessCount';
 import Card from './Card'
 
-
-class App extends React.Component {
+class App extends Component {
+  handleCardClick(card){
+    console.log(card,'clicked');
+    
+  }
   render() {
+    const won = new Date().getSeconds() % 2
+    
     return (
       <div className="memory">
-        <GuessCount guesses={0} />
-        <Card card="😀" feedback="hidden" />
-        <Card card="🎉" feedback="justMatched" />
-        <Card card="💖" feedback="justMismatched" />
-        <Card card="🎩" feedback="visible" />
-        <Card card="🐶" feedback="hidden" />
-        <Card card="🐱" feedback="justMatched" />
+        <GuessCount guesses={'013535'} />
+        <Card card="😀" feedback="hidden" onClick={this.handleCardClick}/>
+        <Card card="🎉" feedback="justMatched" onClick={this.handleCardClick}/>
+        <Card card="💖" feedback="justMismatched" onClick={this.handleCardClick}/>
+        <Card card="🎩" feedback="visible" onClick={this.handleCardClick}/>
+        <Card card="🐶" feedback="hidden" onClick={this.handleCardClick}/>
+        <Card card="🐱" feedback="justMatched" onClick={this.handleCardClick}/>
+        {won && <p>Gagné !!!</p>}
       </div>
     )
   }
